12e5a326a6e342728ab94e8dac1512c67d011867
Timothy M. Shead <tshead@sandia.gov>
2021-11-16 15:16:52 -0700

    Add support links for PyPi.

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ import re
 
 setup(
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         ],
-    description="Synthetic images for training machine learning models.",
+    description="Library for accessing computer vision training data in Limbo Data Format.",
     install_requires=[
         "imagecat",
         "numpy",
@@ -24,6 +25,14 @@ setup(
     name="limbo-ml",
     packages=find_packages(),
     package_data = {"": ["*.csv"]},
+    project_urls={
+        "Chat": "https://github.com/limbo-ml/user/discussions",
+        "Coverage": "https://coveralls.io/r/limbo-ml/user",
+        "Documentation": "http://limbo-ml.readthedocs.io/",
+        "Issue Tracker": "http://github.com/limbo-ml/user/issues",
+        "Regression Tests": "http://github.com/limbo-ml/user/actions",
+        "Source": "http://github.com/limbo-ml/user",
+    },
     scripts=[
         "bin/limbo-materialize",
         "bin/limbo-stats",
